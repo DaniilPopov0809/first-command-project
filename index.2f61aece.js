ae73001162246265eeca9a0b3f173003b83096e1
DaniilPopov0809 <104567066+DaniilPopov0809@users.noreply.github.com>
2022-11-05 07:51:14 +0000

    Deploying to gh-pages from @ DaniilPopov0809/first-command-project@0319d9ab9445c832eae94bbfc135be042941ccd4 🚀

---
+++ index.2f61aece.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".header").clientHeight;document.documentElement.style.setProperty("--body-padding-top","".concat(e,"px"))}();
+//# sourceMappingURL=index.2f61aece.js.map
---
+++ index.d942154d.js
@@ -0,0 +1,2 @@
+let e=document.querySelector(".header").clientHeight;document.documentElement.style.setProperty("--body-padding-top",`${e}px`);
+//# sourceMappingURL=index.d942154d.js.map
